c20414778c2da36bb92da08ce103bd55094d84cd
Pranjal250700 <pranjal@sortstring.com>
2024-10-15 23:16:22 +0530

    changes in some files

--- youtube_data_fetch/manage.py
+++ youtube_data_fetch/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youbtube_data_fetch.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_data_fetch.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- youtube_data_fetch/youtube_data_fetch/urls.py
+++ youtube_data_fetch/youtube_data_fetch/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for youbtube_data_fetch project.
+URL configuration for youtube_data_fetch project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
